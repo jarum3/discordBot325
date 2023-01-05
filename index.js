753f914cecbc263a09884aaa636c922bb423cff4
aiba2 <jarum@umich.edu>
2023-01-05 11:14:39 -0500

    Intents

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, IntentsBitField } = require('discord.js');
+const botIntents = new IntentsBitField();
+botIntents.add(IntentsBitField.Flags.GuildMessages);
+const client = new Client({ intents: botIntents });
+
 
 client.on('ready', () => {
   console.log(`logged in as ${client.user.tag}!`);
