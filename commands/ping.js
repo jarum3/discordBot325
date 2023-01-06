fdc8ac7583ebbb0001ec3dbf1766bc7ba37c1b9b
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-01-06 10:36:34 -0500

    Providing timer for ping

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
   async execute(interaction) {
-    await interaction.reply('Pong!');
+    const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+    interaction.editReply(`Pong!\nTook ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
   },
 };
